4158602e5500db73822c7461574d4f5d3849ddf2
arjunc369 <arjunc369@gmail.com>
2024-03-12 14:55:14 +0530

    push1

--- lib/app_cached_network_image.dart
+++ lib/app_cached_network_image.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
 
 class AppCachedNetworkImage extends StatelessWidget {
   final Widget? loadingWidget;
@@ -25,16 +26,27 @@ class AppCachedNetworkImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.all(Radius.circular(radius)),
-      child: CachedNetworkImage(
-        height: height,
-        width: width,
-        fit: fit,
-        imageUrl: imageUrl,
-        placeholder: (context, url) =>
-            loadingWidget ?? const Center(child: CircularProgressIndicator()),
-        errorWidget: (context, url, error) =>
-            errorWidget ?? const Icon(Icons.error),
-      ),
+      child: Container(
+        child: PhotoView(
+          imageProvider: CachedNetworkImageProvider(
+            imageUrl,
+            maxHeight: height,
+            maxWidth: width,
+          ),
+        )
+        /*
+        child: CachedNetworkImage(
+          height: height,
+          width: width,
+          fit: fit,
+          imageUrl: imageUrl,
+          placeholder: (context, url) =>
+          loadingWidget ?? const Center(child: CircularProgressIndicator()),
+          errorWidget: (context, url, error) =>
+          errorWidget ?? const Icon(Icons.error),
+        ),
+        */
+      )
     );
   }
 }
